001eb44828faf0c1bd90f50eec9e6b96faa0227d
1595728220 <1595728220@qq.com>
2019-03-10 16:40:38 +0800

    create function to finish login

--- public/js/func_login.js
+++ public/js/func_login.js
@@ -1,41 +1,51 @@
-function func_login(){
+function func_login(button,checkElement=null) {
+    console.log(checkElement)
     button.onclick = function (e) {
         e.preventDefault();
         console.log("按钮被点击")
         var name = uname.value;
         var pwd = upwd.value;
-        var check_value = check.checked;
+        var check_value = localStorage.getItem("check");
+        if(checkElement) check_value = check.checked;
         var Reg = /^\w{6,12}$/
-
+        // console.log(check_value)
         if (!Reg.test(name)) {
             alert("用户名格式不正确")
             uname.value = null
+            upwd.value = null
             return
         }
         if (!Reg.test(pwd)) {
             alert("密码格式不正确")
+            uname.value = null
             upwd.value = null
             return
         }
         // console.log(check_value)
-        console.log(name, pwd)
+        // console.log(name, pwd)
         get(`http://127.0.0.1:8080/login?uname=${name}&upwd=${pwd}`).then(function (result) {
             if (result.code === 1) {
+                console.log("登录成功")
                 alert(result.msg)
-                if (check_value) {
-                    localStorage.setItem("uname", name)
-                    localStorage.setItem("check", check)
-                } else {
-                    localStorage.clear();
-                }
+                checkF(name,check_value)
                 upwd.value = null
-                window.location.href = "index.html"
             } else {
+                console.log("登录失败")
                 alert(result.msg)
+                checkF(name,check_value)
                 upwd.value = null
-                uname.value = null
             }
         })
-
+    }
+    function checkF(name,check_value){
+        if (check_value) {
+            localStorage.setItem("uname", name)
+            localStorage.setItem("check", check_value)
+        } else {
+            localStorage.clear();
+            localStorage.setItem("check", check_value)
+            uname.value = null
+        }
     }
 }
+
--- public/js/login.js
+++ public/js/login.js
@@ -83,50 +83,64 @@
 
     //设置按钮点击登录
     function login() {
-        var check = document.getElementById("check")
+        var checkElement = document.getElementById("check")
         var button = document.getElementById("btnFinish")
         console.log(button)
         uname.value = localStorage.getItem("uname")
-        check.checked = localStorage.getItem("check")
-        button.onclick = function (e) {
-            e.preventDefault();
-            console.log("按钮被点击")
-            var name = uname.value;
-            var pwd = upwd.value;
-            var check_value = check.checked;
-            var Reg = /^\w{6,12}$/
+        checkElement.checked = localStorage.getItem("check")
+        //直接调用封装好的验证登录方法
+        func_login(button,checkElement)
+        // button.onclick = function (e) {
+        //     e.preventDefault();
+        //     console.log("按钮被点击")
+        //     var name = uname.value;
+        //     var pwd = upwd.value;
+        //     var check_value = check.checked;
+        //     var Reg = /^\w{6,12}$/
+        //     // console.log(check_value)
+        //     if (!Reg.test(name)) {
+        //         alert("用户名格式不正确")
+        //         uname.value = null
+        //         upwd.value = null
+        //         return
+        //     }
+        //     if (!Reg.test(pwd)) {
+        //         alert("密码格式不正确")
+        //         uname.value = null
+        //         upwd.value = null
+        //         return
+        //     }
+        //     // console.log(check_value)
+        //     // console.log(name, pwd)
+        //     get(`http://127.0.0.1:8080/login?uname=${name}&upwd=${pwd}`).then(function (result) {
+        //         if (result.code === 1) {
+        //             console.log("登录成功")
+        //             alert(result.msg)
+        //             if (check_value) {
+        //                 localStorage.setItem("uname", name)
+        //                 localStorage.setItem("check", check_value)
+        //             } else {
+        //                 localStorage.clear();
+        //                 localStorage.setItem("check", check_value)
+        //             }
+        //             upwd.value = null
+        //             window.location.href = "index.html"
+        //         } else {
+        //             console.log("登录失败")
+        //             alert(result.msg)
+        //             if (check_value) {
+        //                 localStorage.setItem("uname", name)
+        //                 localStorage.setItem("check", check_value)
+        //             } else {
+        //                 localStorage.clear();
+        //                 localStorage.setItem("check", check_value)
+        //                 uname.value = null
+        //             }
+        //             upwd.value = null
+        //         }
+        //     })
 
-            if (!Reg.test(name)) {
-                alert("用户名格式不正确")
-                uname.value = null
-                return
-            }
-            if (!Reg.test(pwd)) {
-                alert("密码格式不正确")
-                upwd.value = null
-                return
-            }
-            // console.log(check_value)
-            console.log(name, pwd)
-            get(`http://127.0.0.1:8080/login?uname=${name}&upwd=${pwd}`).then(function (result) {
-                if (result.code === 1) {
-                    alert(result.msg)
-                    if (check_value) {
-                        localStorage.setItem("uname", name)
-                        localStorage.setItem("check", check)
-                    } else {
-                        localStorage.clear();
-                    }
-                    upwd.value = null
-                    window.location.href = "index.html"
-                } else {
-                    alert(result.msg)
-                    upwd.value = null
-                    uname.value = null
-                }
-            })
-
-        }
+        // }
     }
     //设置点击去注册弹出注册
     function register() {
